46858f3b137b9b8eb8ae0ea5e6606678429f48d8
Ignacio Begue <begueignacio@gmail.com>
2022-11-30 01:11:52 -0300

    fixed toastify functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,6 +11,7 @@ import { CartContextProvider } from "../context/cartContext";
 import Navbar from "./Navbar/Navbar";
 import ItemListContainer from "./ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "./ItemDetailContainer/ItemDetailContainer";
+import { ToastContainer } from "react-toastify";
 import Cart from "./Cart/Cart";
 import Checkout from "./Checkout/Checkout";
 
@@ -24,10 +25,15 @@ const App = () => {
             <Routes>
               <Route path="/" element={<ItemListContainer />} />
               <Route path="/item/:id" element={<ItemDetailContainer />} />
-              <Route path="/category/:category" element={<ItemListContainer />}/>
+              <Route
+                path="/category/:category"
+                element={<ItemListContainer />}
+              />
               <Route path="/cart" element={<Cart />} />
               <Route path="/checkout" element={<Checkout />} />
+              <Route path="*" element={<h1>Ruta no encontrada</h1>} />
             </Routes>
+            <ToastContainer />
           </CartContextProvider>
         </BrowserRouter>
       </LightModeProvider>
